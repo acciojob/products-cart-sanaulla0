c159c42f38770fc700315ac8b1a27b2349a6eda3
sanaulla0 <vadapallisanaulla@gmail.com>
2023-03-31 15:57:43 +0530

    p3

--- src/components/App.js
+++ src/components/App.js
@@ -1,13 +1,18 @@
 
-import React from "react";
-import './../styles/App.css';
+import React,{useState} from "react";
+
 
 const App = () => {
+  const[count,setCount] = useState(0);
+  const getData =()=>{
+          setCount(count+1);
+  }
   return (
     <div>
-        {/* Do not remove the main div */}
+      <p>Button clicked {count} times</p>
+        <button type="submit"onClick={getData} >Click me</button>
     </div>
   )
 }
 
-export default App
+export default App;
